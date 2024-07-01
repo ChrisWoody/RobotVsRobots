c8c2ccd4d7ce039d6a7cee1b53dca1afefb36c4a
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2024-07-01 15:58:48 +0800

    Improved movement, shoot automatically while aiming, added basic enemy as gameplay is looked at next

---
+++ scripts/enemy.gd
@@ -0,0 +1,8 @@
+extends RigidBody3D
+
+
+
+func _physics_process(delta: float):
+	# rotate to look at player
+	# move towards them
+	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-const SPEED := 15.0
+const SPEED := 10.0
 const MOVE_ANGLE := 0.707107
 
 const FIRE_TIMEOUT := 0.15
@@ -25,25 +25,29 @@ func _physics_process(delta: float) -> void:
 
 	var dir := Vector3.ZERO
 	if Input.is_action_pressed("move_left"):
-		dir += Vector3(-MOVE_ANGLE, 0, MOVE_ANGLE)
+		dir = Vector3(-MOVE_ANGLE, 0, MOVE_ANGLE)
 		baseRotation = -45.0
 	if Input.is_action_pressed("move_right"):
-		dir += Vector3(MOVE_ANGLE, 0, -MOVE_ANGLE)
+		dir = Vector3(MOVE_ANGLE, 0, -MOVE_ANGLE)
 		baseRotation = 135.0
 	if Input.is_action_pressed("move_up"):
-		dir += Vector3(-MOVE_ANGLE, 0, -MOVE_ANGLE)
+		dir = Vector3(-MOVE_ANGLE, 0, -MOVE_ANGLE)
 		baseRotation = -135.0
 	if Input.is_action_pressed("move_down"):
-		dir += Vector3(MOVE_ANGLE, 0, MOVE_ANGLE)
+		dir = Vector3(MOVE_ANGLE, 0, MOVE_ANGLE)
 		baseRotation = 45.0
 
 	if Input.is_action_pressed("move_left") and Input.is_action_pressed("move_up"):
+		dir = Vector3(-1, 0, 0)
 		baseRotation = -90.0
 	if Input.is_action_pressed("move_left") and Input.is_action_pressed("move_down"):
+		dir = Vector3(0, 0, 1)
 		baseRotation = 0.0
 	if Input.is_action_pressed("move_right") and Input.is_action_pressed("move_up"):
+		dir = Vector3(0, 0, -1)
 		baseRotation = 180.0
 	if Input.is_action_pressed("move_right") and Input.is_action_pressed("move_down"):
+		dir = Vector3(1, 0, 0)
 		baseRotation = 90.0
 
 	var aimed := false
@@ -70,6 +74,7 @@ func _physics_process(delta: float) -> void:
 
 	if aimed:
 		body.rotation_degrees = Vector3(0.0, bodyRotation, 0.0)
+		shoot(delta)
 
 	if dir != Vector3.ZERO:
 		velocity.x = dir.x * SPEED
@@ -85,37 +90,27 @@ func _physics_process(delta: float) -> void:
 		gunLightElapsed = 0.0
 	gunFlash.light_energy = gunLightElapsed / GUN_LIGHT_TIMEOUT
 
-	# NOTE consider firing when the player aims inside of a separate action
-
-	if Input.is_action_pressed("shoot"):
-		fireElapsed += delta
-
-		if fireElapsed >= FIRE_TIMEOUT:
-			gunLightElapsed = GUN_LIGHT_TIMEOUT
-			fireElapsed = 0.0
-
-			var leftHit = leftArmRayCast.get_collider()
-			var rightHit = rightArmRayCast.get_collider()
-			if leftHit:
-				print("left hit something")
-			else:
-				print("left didn't hit")
-
-			if rightHit:
-				print("right hit something")
-			else:
-				print("right didn't hit")
-
-			var leftBulletTrail: MeshInstance3D = bulletTrail.instantiate()
-			add_sibling(leftBulletTrail)
-			#leftBulletTrail.mesh.size = Vector3(0.05, 0.05, 20)
-			leftBulletTrail.global_rotation = leftArmRayCast.global_rotation
-			leftBulletTrail.global_position = leftArmRayCast.global_position + (leftBulletTrail.basis.z.normalized() * 9)
-
-			var rightBulletTrail: MeshInstance3D = bulletTrail.instantiate()
-			add_sibling(rightBulletTrail)
-			#rightBulletTrail.mesh.size = Vector3(0.05, 0.05, 20)
-			rightBulletTrail.global_rotation = rightArmRayCast.global_rotation
-			rightBulletTrail.global_position = rightArmRayCast.global_position + (rightBulletTrail.basis.z.normalized() * 9)
-
 	move_and_slide()
+
+func shoot(delta: float):
+	fireElapsed += delta
+
+	if fireElapsed >= FIRE_TIMEOUT:
+		gunLightElapsed = GUN_LIGHT_TIMEOUT
+		fireElapsed = 0.0
+
+		var leftSize := 9
+		var leftHit = leftArmRayCast.get_collider()
+		if leftHit:
+			leftSize = 4
+		# var rightHit = rightArmRayCast.get_collider()
+
+		var leftBulletTrail: MeshInstance3D = bulletTrail.instantiate()
+		add_sibling(leftBulletTrail)
+		leftBulletTrail.global_rotation = leftArmRayCast.global_rotation
+		leftBulletTrail.global_position = leftArmRayCast.global_position + (leftBulletTrail.basis.z.normalized() * leftSize)
+
+		var rightBulletTrail: MeshInstance3D = bulletTrail.instantiate()
+		add_sibling(rightBulletTrail)
+		rightBulletTrail.global_rotation = rightArmRayCast.global_rotation
+		rightBulletTrail.global_position = rightArmRayCast.global_position + (rightBulletTrail.basis.z.normalized() * 9)
