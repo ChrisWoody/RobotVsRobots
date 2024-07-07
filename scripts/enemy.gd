c3c65f71e14570f3c18cfaebaf6fb9526e9261f6
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2024-07-07 20:24:21 +0800

    Add basic enemy movement

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,8 +1,18 @@
 extends RigidBody3D
 
+@export var player_path: NodePath
+@onready var player := get_node("../Player")
 
+const ROTATE_TIMEOUT := 0.5
+var rotateElapsed := 0.0
 
-func _physics_process(delta: float):
-	# rotate to look at player
-	# move towards them
-	pass
+func _physics_process(delta: float) -> void:
+	rotateElapsed += delta
+	if rotateElapsed > ROTATE_TIMEOUT:
+		rotateElapsed = 0.0
+		look_at(player.global_position)
+
+	var diff: Vector3 = player.global_position - global_position
+	var norm := diff.normalized()
+	
+	global_position += norm * 3.0 * delta
