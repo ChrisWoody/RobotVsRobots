b557a9a69f294fb04651d8b68efb084de15134eb
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2024-07-10 19:30:37 +0800

    Adding more game logic around enemy spawning behaviour

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,16 +3,24 @@ class_name Enemy extends RigidBody3D
 @export var player_path: NodePath
 @onready var player := get_node("../Player")
 
+signal enemy_death
+
 const ROTATE_TIMEOUT := 0.5
 var rotateElapsed := 0.0
 
-var speed := 3.0
-var health := 10
+var alive := false
+var speed := 0.0
 
-func _ready() -> void:
-	speed = 3.0 + (randf() * 2.0)
+const ORIGINAL_HEALTH := 10
+var health := ORIGINAL_HEALTH
+
+# func _ready() -> void:
+# 	reset_and_hide()
 
 func _physics_process(delta: float) -> void:
+	if !alive:
+		return
+
 	rotateElapsed += delta
 	if rotateElapsed > ROTATE_TIMEOUT:
 		rotateElapsed = 0.0
@@ -23,4 +31,17 @@ func _physics_process(delta: float) -> void:
 func hit() -> void:
 	health -= 1
 	if health <= 0:
+		enemy_death.emit()
 		queue_free()
+
+func spawn(newPosition: Vector3) -> void:
+	speed = 3.0 + (randf() * 2.0)
+	global_position = newPosition
+	visible = true
+	sleeping = false
+	alive = true
+
+# func reset_and_hide() -> void:
+# 	health = ORIGINAL_HEALTH
+# 	visible = false
+# 	sleeping = true
--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -2,16 +2,38 @@ extends Path3D
 
 @export var enemyScene: PackedScene
 @onready var pathFollower: PathFollow3D = $PathFollow3D
+@onready var gameManager: GameManager = get_node("../GameManager")
 
 const SPAWN_TIMEOUT := 2.0
 var spawnElapsed := 0.0
 
+var currentEnemyCount := 0
+const TOTAL_ENEMY_COUNT := 10
+
+var playing := false
+
 func _process(delta: float) -> void:
+	if !playing:
+		return
+
 	spawnElapsed += delta
 
 	if spawnElapsed >= SPAWN_TIMEOUT:
 		spawnElapsed = 0.0
+		if currentEnemyCount >= TOTAL_ENEMY_COUNT:
+			return
+
 		var enemyInstance: Enemy = enemyScene.instantiate()
 		add_sibling(enemyInstance)
 		pathFollower.progress_ratio = randf()
-		enemyInstance.global_position = pathFollower.global_position
+		enemyInstance.enemy_death.connect(on_enemy_death)
+		enemyInstance.spawn(pathFollower.global_position)
+		currentEnemyCount += 1
+
+func on_enemy_death() -> void:
+	currentEnemyCount -= 1
+
+func _on_game_manager_start_game() -> void:
+	currentEnemyCount = 0
+	spawnElapsed = 0.0
+	playing = true
