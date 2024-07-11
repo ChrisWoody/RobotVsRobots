c7219bedaadf4bc802f0caa62177971a5e7bd1c2
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2024-07-11 19:32:22 +0800

    Add basic game UI

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -4,9 +4,12 @@ extends Path3D
 @onready var pathFollower: PathFollow3D = $PathFollow3D
 @onready var gameManager: GameManager = get_node("../GameManager")
 
+signal enemy_destroyed(count: int)
+
 const SPAWN_TIMEOUT := 2.0
 var spawnElapsed := 0.0
 
+var totalEnemiesDestroyed := 0
 var currentEnemyCount := 0
 const TOTAL_ENEMY_COUNT := 10
 
@@ -30,10 +33,14 @@ func _process(delta: float) -> void:
 		enemyInstance.spawn(pathFollower.global_position)
 		currentEnemyCount += 1
 
+# is this being called more than it should? Might be a timing/concurrency issue?
 func on_enemy_death() -> void:
 	currentEnemyCount -= 1
+	totalEnemiesDestroyed += 1
+	enemy_destroyed.emit(totalEnemiesDestroyed)
 
 func _on_game_manager_start_game() -> void:
 	currentEnemyCount = 0
 	spawnElapsed = 0.0
+	totalEnemiesDestroyed = 0
 	playing = true
